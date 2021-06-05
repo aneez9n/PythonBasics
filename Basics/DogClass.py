2d2030d1d22c570f2ce23735489b57dcd899b8da
Aneez ur Rehman <80204329+aneez9n@users.noreply.github.com>
2021-06-05 15:32:37 +0500

    Add files via upload

---
+++ Basics/DogClass.py
@@ -0,0 +1,25 @@
+class Dog:
+    animal = "dog"
+
+    def __init__(self, breed, color):
+        self.breed = breed
+        self.color = color
+
+
+Roger = Dog("Bulldog", "Black")
+Shllby = Dog("Husky", "black and white")
+
+print("Qualities of Roger")
+print("Roger  is a ", Roger.animal)
+print("Roger bread is a ", Roger.breed)
+print("Roger color is ", Roger.color)
+
+print("\nQualities of Shllby")
+print("Shllby is a ", Shllby.animal)
+print("Shllby bread is a ", Shllby.breed)
+print("Shllby color is ", Shllby.color)
+
+
+
+
+
