58cc27366ec89ac4d0d41fb82500e0b2883d7297
Aneez ur Rehman <80204329+aneez9n@users.noreply.github.com>
2021-06-07 18:03:47 +0500

    Add files via upload

---
+++ Basics/LargestNumber.py
@@ -0,0 +1,21 @@
+def largest_number(list_ayaga):
+
+    imaginary_max = list_ayaga[0]
+    for i in list_ayaga:
+        if imaginary_max < i:
+            imaginary_max = i
+    print(imaginary_max)
+
+
+list_agaya = [2, 5, 9, 120, 5, 68]
+largest_number(list_agaya)
+
+
+
+
+
+
+
+
+
+
