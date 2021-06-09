23e2fb5987543582292acafe1b72fdb32572df04
Aneez ur Rehman <80204329+aneez9n@users.noreply.github.com>
2021-06-09 22:37:12 +0500

    Add files via upload

---
+++ Basics/PythonClasses&Objects.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, age):
+        self.age = name
+        self.name = age
+
+    def myfun(self):
+        print(f"hello' my name is {self.name}")
+
+
+object1 = Person("aneez", 22)
+object1.myfun()
+
+
+
